2bc81e431a437190f46f67f9af5e0c3e5b0424ee
Aleksey <Aleksey@DESKTOP-IEN6NTG>
2017-09-30 19:09:06 +0300

    Here i done

--- TD/TD/Tower.h
+++ TD/TD/Tower.h
@@ -7,12 +7,12 @@ class Tower : GameObject
 {
 private:
 	float RangeOfAttack; //Радиус атаки башни
-	int x, y;//индексы хранения башни в массиве
+	int x, y;//индексы хранения башни в массиве asdasdasd
 	int cost;
 	int damage;
 public:
 	Tower();
-	Tower(float RangeOfAttack, int x, int y, int cost, int damage, sf::Shape * shape, sf::Vector2f position);
+	Tower(float RangeOfAttack, int x, int y, int cost, int damage, sf::Shape * shape, sf::Vector2f position); 
 	void DrawTower(sf::RenderWindow & window, sf::Vector2f position); //Малюем башню 
 	Bullet Shoot(int damage, float movementSpeed, sf::Shape &shape, sf::Vector2f position);
 	sf::Vector2f Aim(Enemy enemy); //Проверяем, есть ли враг в радиусе атаки башни. если есть - возвращаем позицию врага что бы стрельнуть по ней
