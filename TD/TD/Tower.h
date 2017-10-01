84e03c4699d2bc7276dc543ff52a2de1893f58e4
Aleksey <Aleksey@DESKTOP-IEN6NTG>
2017-10-01 13:02:51 +0300

    october 1

--- TD/TD/Tower.h
+++ TD/TD/Tower.h
@@ -12,8 +12,7 @@ private:
 	int damage;
 public:
 	Tower();
-	Tower(float RangeOfAttack, int x, int y, int cost, int damage, sf::Shape * shape, sf::Vector2f position); 
-	void DrawTower(sf::RenderWindow & window, sf::Vector2f position); //Малюем башню 
+	Tower(float RangeOfAttack, int x, int y, int cost, int damage, sf::Shape * shape, sf::Vector2f position);
 	Bullet Shoot(int damage, float movementSpeed, sf::Shape &shape, sf::Vector2f position);
 	sf::Vector2f Aim(Enemy enemy); //Проверяем, есть ли враг в радиусе атаки башни. если есть - возвращаем позицию врага что бы стрельнуть по ней
 };
